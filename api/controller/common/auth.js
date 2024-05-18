20b99ad5e44273310fba6732de10e892d8e3c743
Soumya Deep Sarkar <96379749+siddoinghisjob@users.noreply.github.com>
2024-05-18 17:37:22 +0530

    improved logout

--- api/controller/common/auth.js
+++ api/controller/common/auth.js
@@ -3,22 +3,19 @@ const getDataById = require("../../model/ownerModels").getDataById.ac_info;
 const auth = async (req, res) => {
   const results = await getDataById(req.jwtid, req.jwttype);
 
-  if (!results.success)
+  if (!results.success || results.loggedin == 0)
     return res
       .status(401)
       .json({ success: false, msg: ["Invalid credentials."] });
   const data = {
     success: true,
-    type:req.jwttype,
+    type: req.jwttype,
     id: results.id,
     name: results.name,
     email: results.email,
     resume: results?.resume,
     dp: parseInt(req.jwttype) === 0 ? results.logo : results.dp,
-    info:
-      parseInt(req.jwttype) === 0
-        ? results.brand_info
-        : results.bio,
+    info: parseInt(req.jwttype) === 0 ? results.brand_info : results.bio,
   };
   res.status(200).json(data);
 };
