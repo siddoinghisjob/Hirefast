cc99563e63739beb16d2f62d42ee368e406e0280
Soumya Deep Sarkar <96379749+siddoinghisjob@users.noreply.github.com>
2024-05-18 15:50:04 +0530

    added healthz route

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,14 +1,17 @@
 const router = require("express").Router();
 
 const auth = require("../middleware/auth/auth");
-const auth_controller = require("../controller/common/auth"); 
-const logout_controller = require("../controller/common/logout"); 
+const auth_controller = require("../controller/common/auth");
+const logout_controller = require("../controller/common/logout");
 const owner = require("./owner/index");
 const seeker = require("./seeker/index");
 
-router.post("/auth",auth(2), auth_controller);
-router.post("/logout",auth(2), logout_controller);
-router.use("/owner",owner);
-router.use("/seeker",seeker);
+router.get("/healthz", (req, res) =>
+  res.status(200).json({ msg: "Successfull" })
+);
+router.post("/auth", auth(2), auth_controller);
+router.post("/logout", auth(2), logout_controller);
+router.use("/owner", owner);
+router.use("/seeker", seeker);
 
-module.exports =  router;
+module.exports = router;
